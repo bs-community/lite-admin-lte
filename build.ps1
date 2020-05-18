8e9e48f5db8c108bd68c8f606fc388d849f9d790
Pig Fang <g-plane@hotmail.com>
2020-05-18 11:21:35 +0800

    init

---
+++ build.ps1
@@ -0,0 +1,15 @@
+if (!(Test-Path ./dart-sass)) {
+    Invoke-WebRequest 'https://github.com/sass/dart-sass/releases/download/1.26.5/dart-sass-1.26.5-linux-x64.tar.gz' -OutFile ./sass.tar.gz
+    tar -zxf ./sass.tar.gz
+}
+
+npm i
+./dart-sass/sass -q ./admin-lte.scss | npx postcss -o dist/admin-lte.min.css
+
+if (Test-Path ./dist/admin-lte.min.js) {
+    Remove-Item ./dist/admin-lte.min.js
+}
+New-Item -Path ./dist/admin-lte.min.js -ItemType File
+Get-Content ./node_modules/jquery/dist/jquery.min.js | Add-Content ./dist/admin-lte.min.js
+Get-Content ./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js | Add-Content ./dist/admin-lte.min.js
+Get-Content ./node_modules/admin-lte/dist/js/adminlte.min.js | Add-Content ./dist/admin-lte.min.js
