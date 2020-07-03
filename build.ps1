6eba7052f0a8458ef4f372e6fcb40c9f88cb29a3
Pig Fang <g-plane@hotmail.com>
2020-07-03 15:40:32 +0800

    remove source map

--- build.ps1
+++ build.ps1
@@ -12,4 +12,6 @@ if (Test-Path ./dist/admin-lte.min.js) {
 New-Item -Path ./dist/admin-lte.min.js -ItemType File
 Get-Content ./node_modules/jquery/dist/jquery.min.js | Add-Content ./dist/admin-lte.min.js
 Get-Content ./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js | Add-Content ./dist/admin-lte.min.js
-Get-Content ./node_modules/admin-lte/dist/js/adminlte.min.js | Add-Content ./dist/admin-lte.min.js
+$adminLTE = Get-Content ./node_modules/admin-lte/dist/js/adminlte.min.js
+$adminLTE = $adminLTE.Replace('//# sourceMappingURL=adminlte.min.js.map', '')
+Add-Content -Path ./dist/admin-lte.min.js -Value $adminLTE
